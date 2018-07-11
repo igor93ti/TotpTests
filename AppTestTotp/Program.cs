012f76fc7b1f4e34cd3c8a91f3260e14772cbc2e
João Igor Rocha de Oliveira <igor.93.ti@gmail.com>
2018-07-11 00:00:58 -0300

    Inicio dos testes com a biblioteca OtpNet

---
+++ AppTestTotp/Program.cs
@@ -0,0 +1,23 @@
+﻿using OtpNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppTestTotp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] bytes = Encoding.ASCII.GetBytes("ABCDEF34");
+            var totp = new Totp(bytes);
+
+            string codigo = totp.ComputeTotp(DateTime.Now);
+
+            Console.WriteLine(codigo);
+            Console.ReadKey();
+        }
+    }
+}
